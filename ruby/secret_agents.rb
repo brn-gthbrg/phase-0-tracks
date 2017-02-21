d726f19110b475decfb9145ef9919e4862948829
brn-gthbrg <eleanor@gmail.com>
2017-02-20 19:28:08 -0800

    Fixed encrypt and decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,27 +5,24 @@
 # every letter that user puts in the computer will
 # choose the following letter corresponding to next letter in the alphabet
 # if z return to a
+# keep spaces the same
 
 def encrypt(password)
-   encrypted_password = ""
-   index = 0
-  while index < password.length
-      if password[index] == "z"
-      password[index] = "a"
-      encrypted_password = encrypted_password + password[index]
-      index += 1
-      elsif password[index] == " "
-        password[index] = " "
-      index += 1
+  index = 0
+    while index < password.length
+      if password[index] == " "
+        password[index] = password[index]
+      elsif password[index] == "z"
+        password[index] = "a"
       else
-      encrypted_password = encrypted_password + password[index].next
-      index += 1
+        password[index] = password[index].next
       end
-  end
-
-   p encrypted_password
+      index += 1
+    end
+  puts password
 end
 
+# DECRYPT PSEUDO CODE
 # define decrypt(password)
 # Get password
 # Read first letter of password
@@ -34,26 +31,21 @@ end
 
 
 def decrypt(password)
-    decrypt_password = ""
-    i = 0
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-  while i < password.length
-    if password[i] == "a"
-      password[i] = "z"
-      decrypt_password = decrypt_password + password[i]
-    i += 1
-    elsif password[i] == " "
-    i += 1
-    else
-      letter = password[i]
-      position = alphabet.index(letter)
-      position -= 1
-      password[i] = alphabet[position]
-    i += 1
-
+  length = password.length
+  index = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+    while index < password.length
+      if password[index] == " "
+        password[index] = password[index]
+      else
+        character = password[index]
+        position = alphabet.index(character)
+        position = position - 1
+        password[index] = alphabet[position]
+      end
+      index += 1
     end
-  end
-  puts decrypt_password
+  puts password
 end
 
 encrypt("abc")
