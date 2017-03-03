926c1445147370ea886fc5620d741b067c4cca2a
brn-gthbrg <eleanor@gmail.com>
2017-03-02 18:45:33 -0800

    Complete release 1 and 2. Create a class for cars with initialize method and 2 others. Data added to array.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,32 @@ class Puppy
 
 end
 
+
+class Cars
+
+  def initialize
+    puts "This is the initialize method."
+  end
+
+  def tire_turn(numb)
+    numb.times do
+      puts "The tire has turned #{numb} times!"
+    end
+  end
+
+  def ignition
+    puts "Starting up the car"
+  end
+end
+
+cars_arr = []
+
+50.times do
+cars = Cars.new
+cars_arr << cars
+end
+
+cars_arr.each do |car_inst|
+  car_inst.tire_turn(3)
+  car_inst.ignition
+end
