fda4988278280cf7e649f906b37bf9e992234683
brn-gthbrg <eleanor@gmail.com>
2017-03-03 13:36:22 -0800

    First two questions of release 2. celebrate birthday and get mad at methods complete

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,17 +1,45 @@
 class Santa
 
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie)
-    puts "That was a good #{cookie}!"
+    puts "That was a good #{cookie} cookie!"
   end
 
-  def initialize
-    puts "Initializing Santa instance..."
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+      @reindeer_ranking.delete(reindeer)
+      @reindeer_ranking.push(reindeer)
   end
 
 end
 
-puts Santa.new
+# puts Santa.new.eat_milk_and_cookies("chocolate chip")
+
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+p santas
+
+santas.each {|santa| santa.get_mad_at("Vixen")}
+
+p santas
