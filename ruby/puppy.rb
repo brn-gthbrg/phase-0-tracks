4d937663fd809a1360b74db6fe38bee670af1643
brn-gthbrg <eleanor@gmail.com>
2017-03-02 18:18:55 -0800

    Complete release 0. Give puppy tricks

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,1 +1,31 @@
-daphne = Puppy.new
+class Puppy
+
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+
+  def speak(numb)
+    numb.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(int)
+    dog_int = int * 7
+    puts dog_int
+  end
+
+  def shake
+    puts "*lifts paw*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
+end
