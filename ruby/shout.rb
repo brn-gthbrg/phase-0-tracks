26a2a06f15f27a1580a0c9010a8a88e30284ab2e
brn-gthbrg <eleanor@gmail.com>
2017-03-03 19:43:51 -0800

    Complete Release 3. Convert shout module into a mixin module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,40 @@
+# module Shout
+  # def self.yell_angrily(words)
+    # words + "!!!" + " :("
+  # end
+
+  # def self.yell_happily(words)
+    # words + "!" + " :D"
+  # end
+# end
+
+
+#DRIVER CODE
+# Shout.yell_angrily("I'm angry")
+# Shout.yell_happily("I'm happy")
+
+
+
+
+#RELEASE 3: convert to mixin
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-    words + "!" + " :D"
+  def yell_happily(words)
+    words + "!" + ":D"
   end
 end
 
-#DRIVER CODE
-Shout.yell_angrily("I'm angry")
-Shout.yell_happily("I'm happy")
+class Life
+  include Shout
+end
+
+spill_coffee = Life.new
+spill_coffee.yell_angrily("Shit that was hot")
+# spill_coffee.yell_angrily("Shit that was hot")
+
+win_lottery = Life.new
+win_lottery.yell_happily("I'm rich")
+# win_lottery.yell_happily("I'm rich")
