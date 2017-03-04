425e46db93cfdad6ebd6c0d8c76ca02aa6ee9ad3
brn-gthbrg <eleanor@gmail.com>
2017-03-04 11:18:43 -0800

    Release 1 and 2 complete. Create each method and test to confirm they work.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,7 +1,5 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
-puts "List items to add to list separated by a space."
-input = ""
 # steps:
   # take string and separate each word into separate strings
   # set each string as a key within hash table
@@ -9,6 +7,18 @@ input = ""
   # add each item to hash
   # use a block to print each item in the hash
 # output: shopping_list hash
+
+
+def create_list(items)
+  list = items.split
+  shopping_list = {}
+  list.each do |item|
+    shopping_list[item] = 1
+  end
+  shopping_list
+end
+final_list = create_list("banana apple pear")
+puts final_list
 
 # Method to add an item to a list
 # input: list (shopping_list), item name, and optional quantity
@@ -18,6 +28,13 @@ input = ""
   # use push method to add item to hash
 # output: shopping_list with new item added to end
 
+def add_item(list, item, quantity)
+  #list = item.push
+    list[item] = quantity.to_i
+    list
+end
+puts final_list = add_item(final_list, "onions", 2)
+
 # Method to remove an item from the list
 # input: item name, shopping_list
 # steps:
@@ -26,6 +43,12 @@ input = ""
   # Can combine first two steps with delete_if method
 # output: shopping_list without input item
 
+def remove_item(list, item)
+    list.delete(item)
+    list
+end
+puts final_list = remove_item(final_list, "apple")
+
 # Method to update the quantity of an item
 # input: shopping_list, item name, new quantity
 # steps:
@@ -33,8 +56,20 @@ input = ""
   # update the value of quantity for the item
 # output: shopping_list with new quantity for input item
 
+def update_quantity(list, item, quantity)
+    list[item] = quantity.to_i
+    list
+end
+puts final_list = update_quantity(final_list, "banana", 3)
+
 # Method to print a list and make it look pretty
 # input: shopping_list
 # steps:
   # iterate through hash and use puts
 # output: printout of shopping_list
+
+def print_list(grocery_list)
+  grocery_list.each { |item, quantity| puts "You need #{quantity} #{item}"}
+end
+
+print_list(final_list)
