28bdce7da1a54eba7ab6ec7be57c32d4863faea7
brn-gthbrg <eleanor@gmail.com>
2017-03-05 15:59:16 -0800

    Update class and user interface. ls

--- ruby/word_search_game_DBC_6.6/game.rb
+++ ruby/word_search_game_DBC_6.6/game.rb
@@ -1,24 +1,31 @@
 class WordGame
 
-  attr_reader
+  attr_reader :past_guess
   attr_accessor :secret_word, :guess_count
 
   def initialize
     @past_guess = []
-    @past_guess = @input_guess
-    @guess_count = 0
-    @secret_word = ""
   end
 
-  def input_guess(letter)
-    @past_guess << letter
-  end
-
-  def secret_word
+  def begin_game
     puts "What is the secret word?"
     @secret_word = gets.chomp
-    @secret_word_arr = @secret_word.split(//)
+   #  @secret_word_arr = @secret_word.split(//)
     @guess_count = @secret_word.length
+  end
+
+  def guess_recorder(letter)
+    @past_guess = letter
+  end
+
+  def guess_count_updater(letter)
+    if @secret_word.include?(letter)
+      @guess_count -= 1
+    elsif @past_guess.include?(letter)
+      @guess_count
+    else
+      @guess_count -= 1
+    end
   end
 
   def word_hide
@@ -37,22 +44,19 @@ class WordGame
 #  end
 
   def status_update(letter)
-    while @guess_count != 0
       if @secret_word.include?(letter)
+        @blank_word.delete!(" ")
         instances = @secret_word.count(letter)
         instances.times do
-        position = @secret_word[letter]
-        @blank_word.delete(" ")
-        @blank_word.delete(position)
+        position = @secret_word.rindex(letter)
+        @blank_word.slice!(position)
         @blank_word.insert(position.to_i, letter)
-        @secret_word.delete(letter)
         end
-        @guess_count -= 1
-      else @guess_count -=1
-          puts "this message"
       end
-    end
-    puts @blank_word
+    @blank_word
+  end
+
+  def win_lose
   end
 
 end
@@ -62,12 +66,15 @@ end
 puts "Welome to the Word Game"
 game = WordGame.new
 
-game.secret_word
-puts game.guess_count
+game.begin_game
 puts game.word_hide
-puts "Guess a letter"
-guess = gets.chomp
-
-# puts game.letter_check(guess)
-puts game.status_update(guess)
-puts game.guess_count
+# puts game.guess_count
+until game.guess_count == 0
+ # puts game.guess_count
+  puts "Guess a letter"
+  guess = gets.chomp
+  game.guess_recorder(guess)
+  puts game.status_update(guess)
+  game.win_lose
+  game.guess_count_updater(guess)
+end
