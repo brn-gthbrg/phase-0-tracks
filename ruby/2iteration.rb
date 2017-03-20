860da5e3bf84705e13a0987d3929c8dd5af6fd06
brn-gthbrg <eleanor@gmail.com>
2017-03-19 18:09:40 -0700

    Added start/finish message around block for release 0.

--- ruby/2iteration.rb
+++ ruby/2iteration.rb
@@ -1,8 +1,12 @@
 def method(int)
+  puts "Method starting"
   int.to_i.times do |i|
     puts "Print this message for each round and count #{i + 1}."
   end
+  puts "Method ending"
 end
 
 method(3)
+
+
 
