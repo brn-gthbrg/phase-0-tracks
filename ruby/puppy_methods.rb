d4a34e5e3acbc9937b877cff5826a1b2ac1ecfd7
brn-gthbrg <eleanor@gmail.com>
2017-03-22 14:34:26 -0700

    Complete release 0. Add puppy methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,33 +5,54 @@ class Puppy
     toy
   end
 
-end
-
-class Cars
-
-  def initialize
-    puts "This is the initialize method."
+  def speak(numb)
+    numb.times { puts "Woof!" }
   end
 
-  def tire_turn(numb)
-    numb.times do
-      puts "The tire has turned #{numb} times!"
-    end
+  def roll_over
+    puts "*rolls over*"
   end
 
-  def ignition
-    puts "Starting up the car"
+  def dog_years(numb)
+    puts numb * 7
   end
+
+  def shake
+    puts "*sticks out paw*"
+  end
+
 end
 
-cars_arr = []
+Puppy.new.fetch("ball")
+Puppy.new.speak(3)
+Puppy.new.roll_over
+Puppy.new.dog_years(2)
 
-50.times do
-cars = Cars.new
-cars_arr << cars
-end
+# #class Cars
 
-cars_arr.each do |car_inst|
-  car_inst.tire_turn(3)
-  car_inst.ignition
-end
+#  def initialize
+#    puts "This is the initialize method."
+#  end
+
+#   def tire_turn(numb)
+#     numb.times do
+#       puts "The tire has turned #{numb} times!"
+#     end
+#   end
+
+#   def ignition
+#     puts "Starting up the car"
+#   end
+# end
+
+# cars_arr = []
+
+# 50.times do
+# cars = Cars.new
+# cars_arr << cars
+# end
+
+# cars_arr.each do |car_inst|
+#   car_inst.tire_turn(3)
+#   car_inst.ignition
+# end
