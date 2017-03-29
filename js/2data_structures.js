7d15421d6010995b8ac139f8a34b2c89e35d25fe
brn-gthbrg <eleanor@gmail.com>
2017-03-29 15:39:14 -0700

    Complete release 0. Create driver code to add new element to each array.

---
+++ js/2data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "red", "green", "yellow"];
+
+var horses = ["Ed", "Gus", "Sadie", "Secretariat"];
+
+colors.push("purple");
+horses.push("Seabiscuit");
+
+console.log(colors)
+console.log(horses)
