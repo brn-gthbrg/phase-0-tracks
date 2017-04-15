cad8eec742e640c4b6532629523c0633d927e1c6
brn-gthbrg <eleanor@gmail.com>
2017-04-15 12:39:03 -0700

    complete release 0. routes added

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -9,6 +9,7 @@ db.results_as_hash = true
 # add a query parameter
 # GET /
 get '/' do
+  p params
   "#{params[:name]} is #{params[:age]} years old."
 end
 
@@ -43,4 +44,24 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+get '/contact' do
+  "111 Lincoln St. San Jose, CA, 54321"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Great job, #{params[:name]}!"
+  else
+    "Good job!"
+  end
+end
+
+get '/add/:numb1/:numb2' do
+  int1 = params[:numb1].to_i
+  int2 = params[:numb2].to_i
+  sum = int1 + int2
+  p "#{int1} + #{int2} = #{sum}"
 end
